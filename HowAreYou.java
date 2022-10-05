42664d2337cd7b1989ebbb1fcbb4e053ddf380cd
Lisanne <gamingkendoka@gmail.com>
2022-10-05 22:56:21 +0200

    wrote a new startup. Next up is making it looping so that users can try again in case they failed the first time. I also wish to add an exit functionality, and then later the three types.

--- HowAreYou.java
+++ HowAreYou.java
@@ -1,46 +1,67 @@
-import java.util.Scanner; // import the Scanner class 
-import java.util.concurrent.DelayQueue;
+import java.util.Scanner;
+import java.time.LocalDate; // import the LocalDate class
+
+//i want the code to loop back ti the question when IDK finishes
 
 class HowAreYou{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Sup? \n good/bad");
-    
+        System.out.println("Today is " + LocalDate.now());
+
+        System.out.println("Here to rant, read, or stash a question? \n");
+
         String reply = sc.nextLine();
-        String counter = (reply.equals("good"))? "What a day" : "How bad was it on a scale of one to ten?"; 
-        System.out.println(counter);
-        // try{Thread.sleep(10000);}catch(InterruptedException e){System.out.println(e);}  
-        // catch (Exception ie){ System.out.println("Something went wrong");}  
+
         
-        switch (reply) {
-            case "1":
-            System.out.println("I hope it stays at white onions");
-                break;
+        switch(reply){
+            //Case statements  
+            case "rant": System.out.println("Write to your heart's content:");  
+            break;  
+            case "read": System.out.println("On x date you wrote the following:");  
+            break;  
+            case "stash": System.out.println("Ask a question to your future self:");  
+            break;  
+            //Default case statement  
             default:
-            System.out.println("default case");
-                break;
+            IDK();
+            System.out.println("Its gotta be rant, read or stash \n");  
+            }  
+
+        sc.close();        
+    }
+
+    static void IDK(){
+        var value = Math.random() * 10;
+        System.out.println((int)value);
+        switch((int) value){
+            case 0: System.out.println("Could not compute! \\(O.O)/"); break;
+            case 1: System.out.println("Wablief? (O_O)"); break;
+            case 2: System.out.println("I don't speak hooman lang lang"); break;
+            default: System.out.println("Come again?");
         }
-
-
-        sc.close();
-            
-    }
-    String GetReply(){
-        String reply = "";
-        return reply; 
-
     }
 
-    public class Question(){
+
+
+
+
+
+
+
+
+
+    
+    public abstract class Entry{
         String content;
+    }
+
+    public class Question {
         Answer[] answers;
     }
 
-    public class Answer(){
-        String content;
+    public class Answer {
     }
-
 }
 
 
